c839f259fea9f136da946881d283060aa57663f7
MikyGus <57296397+MikyGus@users.noreply.github.com>
2022-01-08 07:04:09 +0100

    Added Simple Factory pattern

---
+++ SimpleFactoryPattern/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SimpleFactoryPattern;
+
+Console.WriteLine("Simple Factory pattern");
+
+SimpleFactory simpleFactory = new SimpleFactory();
+IAnimal prefferedType = simpleFactory.CreateAnimal();
+prefferedType.AboutMe();
---
+++ SimpleFactoryPattern/SimpleFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactoryPattern
+{
+    internal class SimpleFactory
+    {
+        public IAnimal CreateAnimal()
+        {
+            IAnimal intendedAnimal = null;
+            Console.WriteLine("Enter choice: (0:dog, 1:tiger");
+            string b1 = Console.ReadLine();
+            if (int.TryParse(b1,out int input))
+            {
+                Console.WriteLine($"You entered: {input}");
+                switch (input)
+                {
+                    case 0:
+                        intendedAnimal = new Dog();
+                        break;
+                    case 1:
+                        intendedAnimal = new Tiger();
+                        break;
+                    default:
+                        Console.WriteLine("You must enter either 0 or 1");
+                        throw new ApplicationException("Unknown Animal cannot be instantiated.");
+                }
+            }
+            return intendedAnimal;
+        }
+    }
+}
